3506fb8edc3d212a7495d011f0450707e99db094
Burak Şaman <bs@Buraks-MacBook-Air.local>
2023-12-31 11:23:20 +0300

    update

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,138 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+  name: "Burak Şaman",
+  initials: "BŞ",
+  location: "Türkiye, Istanbul",
+  locationLink: "",
+  about: "Frontend Developer",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "Alaylı olarak başladıgım bu sektörde Frontend developer olarak 3+ yılı geçik bir tecrübem var. 3 Farklı firmada çalıştım ve 0 dan 100e birçok proje geliştirdim. canlı sonrası müşteriye destek verdim ve yeni özellikler ekledim.",
+  personalWebsiteUrl: "https://www.buraksmn.dev/",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "buraksmn0@gmail.com",
+    tel: "+90 539 358 3990",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/Buraksmn",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/buraksmn0/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
       },
     ],
   },
-  education: [
-    {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
-    },
-  ],
+  education: [],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      company: "Esa Esports",
+      link: "https://esaesports.com/",
+      badges: ["Remote","React","NextJS","TypeScript"],
+      title: "Mid. Frontend Developer",
+      start: "2023",
+      end: "Halen",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "0 dan kurulan bir ekip ile oyuncuların ilgi alanlarındaki oyunlarda turnuva açıp veya turnuvalara katılabilecegi ready2.gg adında bir proje oluşturduk ve yeni özellikler ile geliştirmelere devam ediyoruz.",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
+      company: "Hub.Studio",
+      link: "",
+      badges: ["React","React Native","NextJs","NX","TypeScript"],
+      title: "Mid. Frontend Developer",
       logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      start: "2022",
+      end: "2022",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Reactjs , ReactNative ve typescript kullanarak kripto para uygulaması geliştirdik.\n" +
+        "Şirketin Kendi içerisinde kullanılan CRM projesinin dönüşüm/yenileme süreçlerinde görev aldım",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
+      company: "Ehesapp & Bermuda",
+      link: "https://ehesapp.com/",
+      badges: ["Remote","ReactJS","Redux"],
+      title: "JR. Frontend Developer → Mid. Frontend Developer",
       logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      start: "2020",
+      end: "2022",
+      description:"B2B ve B2C projeleri geliştirdik. Yurt içi ve yurt dışı projelerinde desteklerde bulundum ve geliştirmeler yaptım. 0-100'e ehesapp projesini geliştirdik ve canlıya aldık. canlı sonrası müşteri taleplerine göre yeni özellikler ekledim.",
     },
   ],
   skills: [
     "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "React/Next.js",
+    "React Native"
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Ready2.gg",
       techStack: [
-        "Side Project",
         "TypeScript",
         "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "Oyuncuların ve Organizatörlerin turnuvalar oluşturup oynatabilecegi bir platform",
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "Ready2.gg",
+        href: "https://Ready2.gg/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+      title: "Ehesapp",
+      techStack: [
+        "React",
+        "Next.js",
+      ],
+      description: "Büyük Firmaların Tüm banka hesaplarını tek bir uygulama üzerinden görüntüleyip rapolar sunan bir uygulama",
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "Ehesapp",
+        href: "https://ehesapp.com/",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+      title: "Sline",
+      techStack: [
+        "React",
+      ],
+      description: "Sadal firmasına geliştirdigimiz bir B2C projesi",
       link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
+        label: "S-line.com.tr",
+        href:"https://s-line.com.tr/",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Buraksmn.dev",
+      techStack: ["Side Project", "Next.js"],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
+        "Kişisel web sitem. Nextjs ve T3Stack kullanılarak geliştirildi.",
+      link: {
+        label: "buraksmn.dev",
+        href: "https://buraksmn.dev/",
+      },
+    },
+    {
+      title: "PurchasingPowerTurkey",
+      techStack: ["Side Project", "Next.js","Radix UI"],
+      description:
+        "2023 Yılının sonunda belli olan yeni asgari ücretin karşısında çalışan kişilerin alım gücünü korumak için ne kadar maaş alması gerektigini hesaplar",
       logo: Minimal,
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "PurchasingPowerTurkey",
+        href: "https://purchasing-power-turkey.vercel.app/",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Tekerlekizi",
+      techStack: ["Side Project", "Next.js", "Ant desing"],
       description:
-        "Generates beautiful wallpapers using random shapes and gradients",
+        "İnsanların çalınan araçlarını paylaşabilecegi ve bulunması sürecinde destek olan kişilere ödül dagıtmayı hedefleyen bir app (geliştirmelere devam ediyorum)",
       logo: BarepapersLogo,
       link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
+        label: "Tekerlekizi",
+        href: "https://tekerlekizi-web.vercel.app/",
       },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
